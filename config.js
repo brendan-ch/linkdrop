e4f274d9aa7df0b55568874fc1f03d840b5f765d
Brendan <brendann.ch@gmail.com>
2020-07-27 19:44:47 -0700

    add config methods

---
+++ config.js
@@ -0,0 +1,43 @@
+const fs = require('fs');
+
+const setConfig = async (config = {}) => {
+  const data = JSON.stringify(config);
+
+  const promise = new Promise((resolve, reject) => {
+    fs.writeFile("config.json", data, (error) => {
+      if (error) {
+        console.log(`Error saving config: ${error}`);
+        reject();
+      };
+  
+      console.log("Config saved successfully.");
+      resolve();
+    });
+  });
+
+  return promise;
+};
+
+const getConfig = () => {
+  let data = undefined;
+
+  try {
+    data = fs.readFileSync("config.json");
+  } catch (e) {  // no config file found, or something else
+    console.log(`Error reading config: ${e}`);
+    return;
+  }
+  
+  if (data) {
+    try {
+      const config = JSON.parse(data);
+      return config;
+    } catch (e) {
+      console.log(`Error parsing config: ${e}`);
+      return;
+    }
+  }
+};
+
+module.exports.getConfig = getConfig;
+module.exports.setConfig = setConfig;
